eddf2488f1d255a35f60ca8781a09f5a91e05a88
rudnytskyi1 <rudnytskyi2007@gmail.com>
2025-11-23 14:29:45 -0600

    Added fixed TElevision class and pytest tests

--- test_television.py
+++ test_television.py
@@ -1,76 +1,72 @@
-import unittest
+import pytest
 from television import Television
 
 
-class TestTelevision(unittest.TestCase):
+class Test:
     def test_init(self):
         television = Television()
-        self.assertEqual(str(television), f'Power = [False], Channel = [0], Volume = [0]')
+        assert str(television) == 'Power = False, Channel = 0, Volume = 0'
 
     def test_power(self):
         television = Television()
         television.power()
-        self.assertEqual(str(television), f'Power = [True], Channel = [0], Volume = [0]')
+        assert str(television) == 'Power = True, Channel = 0, Volume = 0'
         television.power()
-        self.assertEqual(str(television), f'Power = [False], Channel = [0], Volume = [0]')
+        assert str(television) == 'Power = False, Channel = 0, Volume = 0'
 
     def test_mute(self):
         television = Television()
         television.power()
         television.volume_up()
         television.mute()
-        self.assertEqual(str(television), f'Power = [True], Channel = [0], Volume = [1]')
+        assert str(television) == 'Power = True, Channel = 0, Volume = 0'
         television.mute()
-        self.assertEqual(str(television), f'Power = [True], Channel = [0], Volume = [1]')
+        assert str(television) == 'Power = True, Channel = 0, Volume = 1'
         television.power()
+        assert str(television) == 'Power = False, Channel = 0, Volume = 1'
         television.mute()
-        self.assertEqual(str(television), f'Power = [False], Channel = [0], Volume = [1]')
-        television.mute()
-        self.assertEqual(str(television), f'Power = [False], Channel = [0], Volume = [1]')
+        assert str(television) == 'Power = False, Channel = 0, Volume = 1'
 
     def test_channel_up(self):
         television = Television()
         television.channel_up()
-        self.assertEqual(str(television), f'Power = [False], Channel = [1], Volume = [0]')
+        assert str(television) == 'Power = False, Channel = 0, Volume = 0'
         television.power()
         television.channel_up()
-        self.assertEqual(str(television), f'Power = [True], Channel = [2], Volume = [0]')
+        assert str(television) == 'Power = True, Channel = 1, Volume = 0'
         television.channel_up()
         television.channel_up()
-        self.assertEqual(str(television), f'Power = [True], Channel = [0], Volume = [0]')
+        television.channel_up()
+        assert str(television) == 'Power = True, Channel = 0, Volume = 0'
 
     def test_channel_down(self):
         television = Television()
         television.channel_down()
-        self.assertEqual(str(television), f'Power = [False], Channel = [0], Volume = [0]')
+        assert str(television) == 'Power = False, Channel = 0, Volume = 0'
         television.power()
         television.channel_down()
-        self.assertEqual(str(television), f'Power = [True], Channel = [3], Volume = [0]')
+        assert str(television) == 'Power = True, Channel = 3, Volume = 0'
 
-    def tset_volume_up(self):
+    def test_volume_up(self):
         television = Television()
         television.volume_up()
-        self.assertEqual(str(television), f'Power = [False], Channel = [0], Volume = [0]')
+        assert str(television) == 'Power = False, Channel = 0, Volume = 0'
         television.power()
         television.volume_up()
-        self.assertEqual(str(television), f'Power = [True], Channel = [0], Volume = [2]')
+        assert str(television) == 'Power = True, Channel = 0, Volume = 1'
         television.mute()
         television.volume_up()
-        self.assertEqual(str(television), f'Power = [True], Channel = [0], Volume = [2]')
+        assert str(television) == 'Power = True, Channel = 0, Volume = 2'
 
     def test_volume_down(self):
         television = Television()
         television.volume_down()
-        self.assertEqual(str(television), f'Power = [False], Channel = [0], Volume = [0]')
+        assert str(television) == 'Power = False, Channel = 0, Volume = 0'
         television.power()
         television.volume_up()
         television.volume_up()
         television.volume_down()
-        self.assertEqual(str(television), f'Power = [True], Channel = [0], Volume = [1]')
+        assert str(television) == 'Power = True, Channel = 0, Volume = 1'
         television.mute()
         television.volume_down()
-        self.assertEqual(str(television), f'Power = [True], Channel = [0], Volume = [0]')
-
-
-if __name__ == "__main__":
-    unittest.main()
+        assert str(television) == 'Power = True, Channel = 0, Volume = 0'
